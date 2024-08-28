d4ef8c0c0a49521509df615ce68be0d8295fbc41
ThembakaziNgamlana <thembakazingamlana25@gmail.com>
2024-08-28 14:17:30 +0200

    adding react prortfolio

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/profile_img.png'
+
+
+const Hero = () => {
+   return(
+     <div className='hero'>
+       <img src={profile_img} alt=""  />
+       <h1>I'm Thembakazi Ngamlana, Fullstack developer based in South Africa</h1>
+       <p>I am a Fullstack developer from gauteng, I specialised in buliding responsive application that are user friendly.</p>
+       <div className="hero-action">
+       <div className="hero-connect">Connect with me</div>
+       <div className="hero-resume">My resume</div>
+       </div>
+    </div>
+
+   )
+}
+
+
+export default Hero
---
+++ src/Components/Navabar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css'
+import logo from '../../assets/Photoroom.png'
+
+const Navbar = () => {
+   return(
+     <div className='navbar' >
+      <img src={logo} alt="" />     
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>My Work</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect with Me</div>
+     </div>
+
+   )
+}
+
+
+export default Navbar
