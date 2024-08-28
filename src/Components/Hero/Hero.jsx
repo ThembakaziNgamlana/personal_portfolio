df2a20307a393e6869c460d9d71bbad620700161
ThembakaziNgamlana <thembakazingamlana25@gmail.com>
2024-08-28 15:03:38 +0200

    adding react prortfolio

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
    return(
      <div className='hero'>
        <img src={profile_img} alt=""  />
-       <h1>I'm Thembakazi Ngamlana, Fullstack developer based in South Africa</h1>
+       <h1><span>I'm Thembakazi Ngamlana</span>, Fullstack developer based in South Africa</h1>
        <p>I am a Fullstack developer from gauteng, I specialised in buliding responsive application that are user friendly.</p>
        <div className="hero-action">
        <div className="hero-connect">Connect with me</div>
