15466bc5ad577ec9fe18201069e84e2874ef38a1
ThembakaziNgamlana <thembakazingamlana25@gmail.com>
2024-09-03 15:55:09 +0200

    commiting about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from './Components/Navabar/Navbar';
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/about/About";
+import MyWork from "./Components/MyWork/MYWork";
 
 const App = () => {
    return(
@@ -9,6 +10,7 @@ const App = () => {
        <Navbar/>
        <Hero/>
        <About/>
+       <MyWork/>
      </div>
 
    )
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './About.css';
 import profile_img from '../../assets/profile_img.png';
-import Greeting from '../../assets/Greeting.png';
-import Reg from '../../assets/Reg.png';
-import CyberGi from '../../assets/CyberGi.png';
+
 
 const About = () => {
   return (
@@ -18,7 +16,7 @@ const About = () => {
         <div className='about-right'>
           <div className='about-para'>
             <p>I am a passionate and dedicated Full Stack Developer. My journey into the world of web development has been fueled by a strong desire to build innovative and user-friendly applications that solve real-world problems.</p>
-            <p>As a learner, I am committed to mastering both front-end and back-end technologies...</p>
+            <p> As a learner, I am committed to mastering both front-end and back-end technologies. I have been actively involved in projects that require proficiency in HTML, CSS, Java, AngularJs,  and databases like  PostgreSQL. My learnership has provided me with hands-on experience, allowing me to apply theoretical knowledge in a practical environment.</p>
           </div>
           <div className='about-skills'>
             <div className='about-skill'><p>HTML</p><hr style={{width:"50%"}} /></div>
@@ -30,40 +28,7 @@ const About = () => {
         </div>
       </div>
 
-      <div className='about-achivements'>
-        <h1>PROJECTS COMPLETED</h1>
-        <hr />
-        <div className='projects-grid'>
-          <div className='project-item'>
-            <h2>Registration App</h2>
-           
-            <p>This app was built to create a user interface that displays a list of registrations...
-            https://registration-1m0k.onrender.com
-            </p>
-            <img src={Reg} alt="Registration App" />
-          </div>
-          <div className='project-item'>
-            <h2>Greetings App</h2>
-          
-            <p>This app was built to test my skill on a DOM widget with separated front-end...
-              https://greetings-webapp-yi55.onrender.com
-           </p>
-            <img src={Greeting} alt="Greetings App" />
-          </div>
-          <div className='project-item'>
-            <h2>CyberGi App</h2>
-            <p>I have built this app with my team to teach Young professionals about Cyber security...</p>
-            <img src={CyberGi} alt="CyberGi App" />
-          </div>
-          <div className='project-item'>
-            <h2>Church App</h2>
-            <p>I have built this for my Church to help church members to be abkle to see the upcoming events and be send prayer request to the pastor
-             https://github.com/ThembakaziNgamlana/church.git
-            </p>
-            <img src={CyberGi} alt="CyberGi App" />
-          </div>
-        </div>
-      </div>
+     
     </div>
   );
 };
