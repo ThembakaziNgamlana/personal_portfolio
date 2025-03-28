3abb8b6e3fb89c999d554033aeac73daad5c74a9
Thembakazi ngamlana <111046443+ThembakaziNgamlana@users.noreply.github.com>
2025-03-28 11:32:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from './Components/Navabar/Navbar';
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
-import MyWork from "./Components/MyWork/MYWork";
+import MyWork from "./Components/MyWork/MyWork";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 
